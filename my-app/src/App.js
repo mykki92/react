e160241ac00245f3074f5ac8a2b1cd569b4a8841
mykki92 <mike2108@hotmail.co.uk>
2023-11-03 14:58:40 +0000

    stateful greetings

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithPrevState from "./components/StatefulGreetingWithPrevState";
+import StatefulGreetingWithCallback from "./components/StatefulGreetingWithCallback";
+
+
+//function App() {
+//  return (
+//    <div className="App">
+//      <FunctionalGreeting />
+//      <FunctionalGreetingWithProps greeting="fiesty one you are!" name="Mike" age="32" />
+//    </div>
+//  );
+//}
+
+//export default App;
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
-      <FunctionalGreetingWithProps greeting="fiesty one you are!" name="Mike" age="32" />
+      <StatefulGreetingWithPrevState />
     </div>
   );
 }
