b2ada62b244aa2eed8a924c5e8b18f202b5eb394
mykki92 <mike2108@hotmail.co.uk>
2023-11-10 15:42:57 +0000

    forms and hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -21,30 +21,71 @@ import Clock from "./components/Clock";
 import LifeCyclesCDM from "./components/LifeCyclesCDM";
 import LifeCyclesCDU from "./components/LifeCyclesCDU";
 import LifeCyclesCWU from "./components/LifeCyclesCWU";
+import ControlledForm from "./components/ControlledForm";
+import ControlledFormHooks from "./components/ControlledFormHooks";
+import UncontrolledForm from "./components/UncontrolledForm";
+import { SearchBar3 } from "./components/SearchBar3";
+import HooksCounter from "./components/HooksCounter";
+import ClassCounter from "./components/ClassCounter";
+import UseStateWithArrays from "./components/UseStateWithArrays";
 
 function App() {
   return (
     <div className="App">
       <FunctionalGreeting />
+      <hr />
       <FunctionalGreetingWithProps greeting="fiesty one you are!" name="Mike" age="32" />
+      <hr />
       <StatefulGreeting greeting="I'm a stateful class component!" name="Mike"/>
+      <hr />
       <StatefulGreetingWithPrevState />
+      <hr />
       <StatefulGreetingWithCallback />
+      <hr />
       <NavBarForm />
+      <hr />
       <Sidebar />
+      <hr />
       <EventsFunctional />
+      <hr />
       <EventsClass />
+      <hr />
       <EventBinding />
+      <hr />
       <ConditionalRenderingClass />
+      <hr />
       <ConditionalRenderingFunctional connected={true} />
+      <hr />
       <NestingComponents />
+      <hr />
       <MethodsAsPropsParent />
+      <hr />
       <Clock />
+      <hr />
+      <ControlledForm />
+      <hr />
+      <ControlledFormHooks />
+      <hr />
+      <UncontrolledForm />
+      <hr />
+      <HooksCounter />
+      <hr />
+      <ClassCounter />
+      <hr />
+      <UseStateWithArrays />
+      <hr />
+      <SearchBar3 />
+      <hr />
       <LifeCyclesCDM />
+      <hr />
       <LifeCyclesCDU />
+      <hr />
       <LifeCyclesCWU />
+      <hr />
       <RenderingLists />
+      <hr />
       <Content />
+      <hr />
     </div>
   );
 }
